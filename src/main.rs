333c833110ab0696c1775ee6eeb84fe79faaca47
a <a@b.com>
2021-03-20 00:27:59 -0400

    Building match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101).to_string();
+    println!("The secret number is {}", secret_number);
+
+    println!("Pleas input yout guess. ");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+        }
 }
